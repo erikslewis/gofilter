089b888d3336fd6fd5cf09762d84f3517ba7a13f
mwb86 <masonwbrown@gmail.com>
2017-01-25 10:28:40 -0500

    ui router

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var app = angular.module('goFilter', []);
-
+var app = angular.module('goFilter', ['ui.router']);
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl:'/home.html',
+      controller: 'MainCtrl'
+    });
+    $urlRouterProvider.otherwise('home');
+  }]);
 app.factory('posts', [function(){
   var o = {
     posts: []
